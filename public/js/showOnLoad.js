4e85a14d75cd5375f511c47c441188d7ca8dd1b9
simotasca <simo.tasca@gmail.com>
2022-02-26 18:34:50 +0100

    window onload

--- public/js/showOnLoad.js
+++ public/js/showOnLoad.js
@@ -4,7 +4,7 @@
 //   visibility: hidden; 
 // }
 
-document.addEventListener("load", () => {
+window.addEventListener("load", () => {
   Array.from(document.getElementsByClassName('show-on-load')).forEach(function (element) {
     element.classList.remove('show-on-load');
   });
