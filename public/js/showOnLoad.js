8b47a38261c3f850ae5f4ce44111554e25f729e6
simotasca <simo.tasca@gmail.com>
2022-02-27 15:25:35 +0100

    devo testare online

--- public/js/showOnLoad.js
+++ public/js/showOnLoad.js
@@ -4,8 +4,8 @@
 //   visibility: hidden; 
 // }
 
-window.addEventListener("load", () => {
-  Array.from(document.getElementsByClassName('show-on-load')).forEach(function (element) {
-    element.classList.remove('show-on-load');
-  });
-});
+// window.addEventListener("load", () => {
+//   Array.from(document.getElementsByClassName('show-on-load')).forEach(function (element) {
+//     element.classList.remove('show-on-load');
+//   });
+// });
