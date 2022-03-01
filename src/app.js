a4af5bcfedbf4e9db9da219066a855dbac515235
simotasca <simo.tasca@gmail.com>
2022-03-01 12:09:06 +0100

    sistemorobe

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 function sendPage(res, page) {
-  res.sendFile(path.join(__dirname, "..", "public", page));
+  res.sendFile(path.join(__dirname, page));
 }
 
 // routes
