b852ae76423f681d73a12c88d92ae194fc1b5de6
simotasca <simo.tasca@gmail.com>
2022-03-01 12:20:36 +0100

    ordine

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 function sendPage(res, page) {
-  res.sendFile(path.join(__dirname, "..", "public", "pages", page));
+  res.sendFile(path.join(__dirname, "pages", page));
 }
 
 // routes
